c435ae868e1abb060cce72fe9b041fe12e8d0d01
Kynan Justis <kfjustis@gmail.com>
2018-01-16 17:32:40 -0600

    separated into parts for assignment

---
+++ part-b/build/CMakeFiles/part_b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/part_b.dir/example1.cpp.o"
+  "part_b.pdb"
+  "part_b"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/part_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
